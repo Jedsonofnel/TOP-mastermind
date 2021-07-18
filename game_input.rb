57c148ade9d0771b687c2cd4bd8568574c1464f9
jedn <jed@nelson.ac>
2021-07-19 00:08:53 +0200

    Change game_input into a class

--- game_input.rb
+++ game_input.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
 # functions relating to getting input for the game
-module GameInput
+class GameInput
   # Asks for and gets the name of the human player
   def player_name_input
     print "What is your name?\n> "
-    gets.chomp
+    @player_name = gets.chomp
+  end
+
+  # Asks for and gets whether the player wants to be the guesser or coder
+  def gameplay_type_input
+    print '> '
+
+    case gets.chomp.downcase
+    when 'g'
+      'code_guesser'
+    when 'c'
+      'code_creator'
+    else
+      puts 'Invalid input!'
+      gameplay_type_input
+    end
   end
 end
