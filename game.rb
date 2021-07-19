cf74e1366dc13f311ab610ee158eff8909a1f33c
jedn <jed@nelson.ac>
2021-07-19 15:33:23 +0200

    Implement code-guessing gameplay loop

--- game.rb
+++ game.rb
@@ -19,9 +19,24 @@ class Game
     gameloop
   end
 
+  def bulls_and_cows(guess, code = @current_code)
+    output = Hash.new(0)
+
+    code.split('').each_with_index do |digit, index|
+      if guess.split('')[index] == digit
+        output['bulls'] += 1
+      elsif code.split('').include? guess.split('')[index]
+        output['cows'] += 1
+      end
+    end
+
+    output
+  end
+
   private
 
   def gameloop
+    @current_code = generate_code
     show_rules
 
     case gameplay_type_input
@@ -35,29 +50,44 @@ class Game
   end
 
   def guesser_gameplay
-    # Do things
+    guess_number = 0
+    result = {}
+    7.times do
+      guess_number += 1
+      result = bulls_and_cows(guess_input(guess_number))
+      puts result + "\n"
+      break if correct_code?(result)
+    end
+
+    correct_code?(result) ? guess_win_message(guess_number) : guess_lose_message
   end
 
   def creator_gameplay
     # Do things
   end
 
-  def bulls_and_cows(code, guess)
-    # Return Hash of ("bulls" => x, "cows" => y)
-    output = Hash.new(0)
-
-    code.split('').each_with_index do |digit, index|
-      if guess.split('')[index] == digit
-        output['bulls'] += 1
-      elsif guess.split('').include? digit
-        output['cows'] += 1
-      end
-    end
-
-    output
-  end
-
   def goodbye
     puts "Goodbye #{@player_name}!"
+  end
+
+  def guess_win_message(guess_number)
+    puts "Congratulations #{@player_name} - you won in #{guess_number} turns!"
+    @scoreboard[@player_name] += 1
+  end
+
+  def guess_lose_message
+    puts "#{@player_name}, you were unable to guess the code!  It was #{@current_code}."
+    @scoreboard['Computer'] += 1
+  end
+
+  def generate_code
+    code = ''
+    characters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].shuffle
+    4.times { code += characters.pop.to_s }
+    code
+  end
+
+  def correct_code?(bulls_and_cows_hash)
+    bulls_and_cows_hash['bulls'] == 4
   end
 end
