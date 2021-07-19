2ced263e0025da924f22490f7af8c76176a6af7b
jedn <jed@nelson.ac>
2021-07-19 09:34:01 +0200

    Lay out class structure

--- game.rb
+++ game.rb
@@ -3,12 +3,31 @@
 require_relative 'game_input'
 require_relative 'rules'
 
-# This class plays games of mastermind
+# This class plays games of Mastermind
 class Game
-  include GameInput
   include Rules
 
+  def initialize
+    @player_interraction = GameInput.new
+  end
+
   def play
-    player_name = player_name_input
+    @player_interraction.player_name_input
+    show_rules
+    gameloop
+  end
+
+  def gameloop
+    # Asks what type of game player wants
+    # Then plays that type of game
+    # Then asks if player wants to do it again
+  end
+
+  def guesser_gameplay
+    # Do things
+  end
+
+  def creator_gameplay
+    # Do things
   end
 end
