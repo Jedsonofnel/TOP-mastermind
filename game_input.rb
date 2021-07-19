6790c01b9befa76ad930dfb183d780e6ec225165
jedn <jed@nelson.ac>
2021-07-19 12:10:05 +0200

    Add method to ask whether user wants to play again

--- game_input.rb
+++ game_input.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 # functions relating to getting input for the game
-class GameInput
+module GameInput
   # Asks for and gets the name of the human player
   def player_name_input
     print "What is your name?\n> "
-    @player_name = gets.chomp
+    gets.chomp
   end
 
   # Asks for and gets whether the player wants to be the guesser or coder
   def gameplay_type_input
-    print '> '
+    print "Would you like to be the code creator or code guesser? (g/c)\n> "
 
     case gets.chomp.downcase
     when 'g'
@@ -20,6 +20,22 @@ class GameInput
     else
       puts 'Invalid input!'
       gameplay_type_input
+    end
+  end
+
+  # Asks whether the player wants to play again
+  # Returns true or false accordingly
+  def play_again?
+    print "Would you like to play again? (y/n)\n> "
+
+    case gets.chomp.downcase
+    when 'y'
+      true
+    when 'n'
+      false
+    else
+      puts 'Invalid input!'
+      play_agian?
     end
   end
 end
